4f12e60688cf1f1df38f34f22e411883e2dd0457
sohankyatham <your_email_linked_to_github@example.com>
2025-11-12 11:58:30 -0500

    Added user interface implementation for analysis

--- scripts/analyze_current_data.py
+++ scripts/analyze_current_data.py
@@ -3,15 +3,16 @@ analyze_current_data.py
 Analyze current data from LabView CSV files for Y-PENG research.
 
 Goal of script:
-find maximum, minimum, mean, and peak current
-peak = max - min
+find maximum, minimum, mean, and peak_to_peak current
+peak_to_peak_current = max current - min current (difference between max & min values)
+find average current 
 
 OUTLINE:
 
 1. Imports and Input file (UI .csv file selector in data folder instead of manually hardcoding in code)
 2. Load PENG data (exception handling with tkinter)
 3. Plot results (make sure to convert to readable units) - add labels and title
-4. Find max, min, mean current value and find peak (max - min) & display results in tkinter window 
+4. Find max, min, mean current value and find peak_to_peak (max - min) & display results in tkinter window 
 5. In tkinter output window have option to save results in a file 
 '''
 
@@ -19,6 +20,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
+from tkinter import filedialog, messagebox 
+import os
+
 
 
 '''
@@ -26,7 +30,7 @@ Define functions - add implementation soon
 '''
 
 def get_filepath():
-    '''Get the .csv data file that needs to be analyzed'''
+    '''File dialog to get the .csv data file that needs to be analyzed'''
     pass
 
 def load_data():
@@ -34,20 +38,38 @@ def load_data():
     pass
 
 def analyze_current():
-    '''Return min, max, mean, and peak current values'''
+    '''Return min, max, mean, and peak_to_peak current values'''
     pass
 
 def get_results():
-    '''Use tkinter to show analysis and have option to save'''
+    '''Use matplotlib & tkinter to show analysis and have option to save'''
+    '''x-axis: Time (s) and y-axis: Current (μA)'''
     pass
 
 def save_results():
     '''Save the results in a file'''
     pass
 
+def launch_analysis_ui():
+    """The main Tkinter user interface for analyzing current data."""
+    root = tk.Tk()
+    root.title("Y-PENG Current Analyzer")
+    root.geometry("600x600")
+
+    label = tk.Label(root, text="Select a CSV file to analyze:")
+    label.pack(pady=10)
+
+    select_button = tk.Button(root, text="Browse CSV")
+    select_button.pack(pady=5)
+
+    analyze_button = tk.Button(root, text="Analyze")
+    analyze_button.pack(pady=20)
+
+    root.mainloop()
+
 def main():
     '''Entry Point: Main function to load, analyze, and save results'''
-    pass
+    launch_analysis_ui()
 
 if __name__ == "__main__":
     main()
