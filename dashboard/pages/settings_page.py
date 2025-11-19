561940aeca1a807e2798aed293d37b4e483d7b48
sohankyatham <Sohan.Kyatham23@gmail.com>
2025-11-18 20:06:03 -0500

    Planning for settings page

---
+++ dashboard/pages/settings_page.py
@@ -0,0 +1,38 @@
+'''
+Handle uploaded YPENG data
+
+Default sampling rate
+
+Default column names (force, voltage, displacement, etc.)
+
+Apply smoothing by default? (checkbox)
+
+Select smoothing algorithm (moving average, Savitzky–Golay, etc.)
+
+Default window size for plotting
+
+
+
+
+VISUALIZATION settings
+Theme (light / dark / auto)
+
+Line thickness
+
+Show gridlines?
+
+Enable interactive hover info
+
+Default plot color scheme
+
+
+
+
+User Preferences
+Units preference (e.g., mV vs V)
+Enable debugging mode
+
+Show/hide advanced features
+
+Disable animations (for performance)
+'''
